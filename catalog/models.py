bb4ebfea53941134c5ae28a48e96980c923c63e0
damián 141 <deimian141@gmail.com>
2023-12-17 15:48:25 -0500

    creación de archivos en mi repositorio

---
+++ catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Catalog(models.Model):
+    nombre = models.CharField(max_length=30)
+
+
+from django.db import models
+
+# Create your models here.
---
+++ pokemon1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Pokemon1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pokemon1'
